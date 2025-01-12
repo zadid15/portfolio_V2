e3a08d2a32687eccfa09ef07abf07aeac1f127f3
zadid15 <muhammadzadid061512@gmail.com>
2025-01-12 10:14:36 +0700

    fix errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.html"  // Menambahkan pencarian di root proyek untuk file HTML
+  ],
   theme: {
     extend: {},
   },
